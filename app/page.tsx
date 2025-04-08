e2f1377b2ac922d92ba1b7d82c88ecbee1b73be2
Nabil <lahlalia.nabil@hotmail.com>
2025-04-08 19:18:25 +0100

    first c

---
+++ app/page.tsx
@@ -0,0 +1,75 @@
+import { cars as allCars } from "@data/data";
+import { HomeProps } from "@types";
+import { fuels, yearsOfProduction } from "@constants";
+import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@components";
+
+export default async function Home({ searchParams }: HomeProps) {
+ // Simulate filtering
+ const filteredCars = allCars.filter((car) => {
+  const matchManufacturer = searchParams.manufacturer
+    ? car.make.toLowerCase().includes(searchParams.manufacturer.toLowerCase())
+    : true;
+
+  const matchYear = searchParams.year
+    ? car.year === Number(searchParams.year)
+    : true;
+
+  const matchFuel = searchParams.fuel
+    ? car.fuel_type.toLowerCase() === searchParams.fuel.toLowerCase()
+    : true;
+
+  const matchModel = searchParams.model
+    ? car.model.toLowerCase().includes(searchParams.model.toLowerCase())
+    : true;
+
+  return matchManufacturer && matchYear && matchFuel && matchModel;
+});
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
+  return (
+    <main className='overflow-hidden'>
+      <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' options={fuels} />
+            <CustomFilter title='year' options={yearsOfProduction} />
+          </div>
+        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {filteredCars.slice(0, searchParams.limit || 10).map((car, index) => (
+                <CarCard key={index} car={car} />
+              ))}
+            </div>
+            {/* <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div> */}
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}
---
+++ data/data.ts
@@ -0,0 +1,78 @@
+export const cars = [
+    {
+      city_mpg: 29,
+      class: "Sedan",
+      combination_mpg: 32,
+      cylinders: 4,
+      displacement: 2.0,
+      drive: "FWD",
+      fuel_type: "Gas",
+      highway_mpg: 37,
+      make: "Honda",
+      model: "Civic",
+      transmission: "Automatic",
+      year: 2022,
+      carImg: "/assets/honda-civic.webp"
+    },
+    {
+      city_mpg: 26,
+      class: "SUV",
+      combination_mpg: 28,
+      cylinders: 4,
+      displacement: 2.5,
+      drive: "AWD",
+      fuel_type: "Fuel",
+      highway_mpg: 33,
+      make: "Mazda",
+      model: "CX-5",
+      transmission: "Automatic",
+      year: 2023,
+      carImg: "/assets/mazda_cx.webp"
+    },
+    {
+      city_mpg: 124,
+      class: "Sedan",
+      combination_mpg: 120,
+      cylinders: 0,
+      displacement: 0.0,
+      drive: "RWD",
+      fuel_type: "Electricity",
+      highway_mpg: 112,
+      make: "Tesla",
+      model: "Model 3",
+      transmission: "Automatic",
+      year: 2023,
+      carImg: "/assets/tesla_model.webp"
+    },
+    {
+      city_mpg: 53,
+      class: "Hatchback",
+      combination_mpg: 52,
+      cylinders: 4,
+      displacement: 1.8,
+      drive: "FWD",
+      fuel_type: "Hybrid",
+      highway_mpg: 48,
+      make: "Toyota",
+      model: "Prius",
+      transmission: "CVT",
+      year: 2021,
+      carImg: "/assets/toyota_prius.webp"
+    },
+    {
+      city_mpg: 28,
+      class: "Compact",
+      combination_mpg: 30,
+      cylinders: 4,
+      displacement: 1.6,
+      drive: "FWD",
+      fuel_type: "Gas",
+      highway_mpg: 34,
+      make: "Kia",
+      model: "Forte",
+      transmission: "Automatic",
+      year: 2022,
+      carImg: "/assets/kia_forte.webp"
+    }
+  ];
+  
